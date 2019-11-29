fa4f4dbde11ce43689e76a4c275080feb8c2f3af
Tahj <tahjchisolm21@gmail.com>
2019-11-28 21:06:43 -0500

    set N and M

--- project3.s
+++ project3.s
@@ -4,3 +4,11 @@ invalidMessage: .asciiz "NaN"
 input: .space 1001
 
 .text
+
+main:
+    li $t0, 2839480
+    li $t1, 11
+    div $t0, $t1
+    mfhi $t0 # store remainder
+    addi $s0, $t0, 26 # base N
+    addiu $s1, $s0, -10 # M
