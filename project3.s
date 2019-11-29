fd935d42a00cb3b9fde03d0c4e35c03e5e3bf564
Tahj <tahjchisolm21@gmail.com>
2019-11-28 23:35:24 -0500

    started subprg C

--- project3.s
+++ project3.s
@@ -211,3 +211,12 @@ addInt:
 
 back:
     jr $ra
+
+subprgC:
+
+    li $s7, 65
+    add $s7, $s1, $s7
+    sgt $t8, $a1, 64
+    slt $a3, $a1, $s7
+    and $t8, $t8, $a3
+    bne $t8, $zero, uppercase
